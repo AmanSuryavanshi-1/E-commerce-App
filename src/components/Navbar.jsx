32ea9b787435c7dc09e2b7a864d04b7c0991881f
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-04-08 22:45:23 +0530

    3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
+import myLogo from './LogoAS.png';
 
 const Container = styled.div`
     height: 60px;
@@ -9,11 +11,13 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding:10px 20px;
     display: flex;
+    align-items : center;
     justify-content: space-between;
+    background-color: grey;
 `
 const Left = styled.div`
-flex:1;
-display:flex; 
+flex: 1;
+display: flex; 
 align-items: center;
 `;
 
@@ -23,15 +27,38 @@ const Language = styled.span`
 `;
 
 const SearchContainer =styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
     display: flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px;
 `;
+
+const Input = styled.input`
+    border:none;
+`;
+
 const Center = styled.div`
-flex:1 `;
+    flex:1; 
+    text-align: center;
+`;
+
+// const Logo = styled.image`
+
+// `;
 
 const Right = styled.div`
-flex:1 `;
+    flex:1; 
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+    font-size:14px;
+    cursor:pointer;
+    margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
@@ -40,12 +67,22 @@ const Navbar = () => {
             <Left>
                 <Language>EN</Language>
                 <SearchContainer>
-                    input
-                    <Search/>
+                    <Input/>
+                    <Search style={{color:"gray", fontSize: "16px"}}/>
                 </SearchContainer>
             </Left>
-            <Center>center</Center>
-            <Right>right</Right>
+
+            <Center>< img src={myLogo} alt='logo' height={"30px"} width={"120px"}/></Center>
+            
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlined />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
         </Container>
   )
---
+++ src/components/Slider.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display:flex;
+`;
+
+const Slider = () => {
+  return (
+    <Container>Slider</Container>
+  )
+}
+
+export default Slider
