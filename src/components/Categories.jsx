572dc7728ecf96f82329e82fa2f752d450778357
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-04-12 22:58:52 +0530

    3.6

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,21 @@
-import { Container } from '@material-ui/core'
+import styled from 'styled-components'
 import { categories } from '../data'
 import CategoryItem from './CategoryItem'
+
+
+const Container = styled.div`
+display: flex;
+`;
 
 const Categories = () => {
   return (
     <Container> 
         {categories.map(item=>(
-            <CategoryItem/>
+          // so i can map through my array and for each item i am gonna call my category item component
+            <CategoryItem item={item}/>
+            //so ass prop i will give my item
+            // so i can take this prop in categoryItem.jsx and can use it inside my container
+
         ))}
     </Container>
   )
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
-
 import styled from "styled-components";
 
-const Container = styled.div``
 
-const CategoryItem = () => {
+
+const Container = styled.div``;
+
+const CategoryItem = ({item}) => {
   return (
-    <Container>CategoryItem</Container>
+    <Container>
+
+    </Container>
   )
 }
 
