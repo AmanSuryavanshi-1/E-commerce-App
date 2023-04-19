43b19378f0fff2147210c1e80438ca30263158ac
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-04-19 14:48:46 +0530

    19.2/4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
-// import Register from "./pages/Register";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 
 const App = () => {
   return <Home/>
 
+  // <Home/>
   // <Login/>
   // <Register/>;
   // <Product/>
